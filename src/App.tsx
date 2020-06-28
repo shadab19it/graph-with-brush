75d5293c43daf8c696c26c25ddccbcb2dd20ff42
Shadab <shadab19it@gmail.com>
2020-06-28 12:09:24 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Graph from "./components/Graph/Graph";
+import { ParentSize } from "@vx/responsive";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "0 auto",
+        width: "900px",
+        height: "200px",
+        paddingTop: "100px",
+      }}>
+      <ParentSize>{({ width, height }) => <Graph width={width} height={height} />}</ParentSize>
     </div>
   );
-}
+};
 
 export default App;
